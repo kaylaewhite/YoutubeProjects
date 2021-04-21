459b061339a053e98d7dea46a5db43d3105feb7f
kaylaewhite <81335950+kaylaewhite@users.noreply.github.com>
2021-04-21 16:51:16 -0400

    Add files via upload

---
+++ largestnumberlist.py
@@ -0,0 +1,6 @@
+numbers = [44, 21, 67, 43, 7, 18, 20]
+largest = numbers[0]
+for number in numbers:
+    if number > largest:
+        largest = number
+print(largest)
---
+++ writingwithx.py
@@ -0,0 +1,3 @@
+numbers = [2, 2, 2, 2, 6]
+for number in numbers:
+    print(number * "x")
