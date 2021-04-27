2f7274cf57bd5d24f321223e49ce2764ec23d759
kaylaewhite <81335950+kaylaewhite@users.noreply.github.com>
2021-04-27 15:29:13 -0400

    Add files via upload

---
+++ dictionarypractice.py
@@ -0,0 +1,17 @@
+number = input("Phone: ")
+conversion = {
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+    "0": "Zero"
+}
+output = ""
+for ch in number:
+    output += conversion.get(ch, "Batman!") + " "
+print(output)
